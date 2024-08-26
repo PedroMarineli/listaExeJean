ac8ea5eb02275ee2ccd1466dfa223b83ca64d236
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2024-08-26 10:59:59 -0300

    repeticao

--- src/main/java/repeticao/Exercicio6.java
+++ src/main/java/repeticao/Exercicio6.java
@@ -1,7 +1,17 @@
 package repeticao;
 
+import java.util.Scanner;
+
 public class Exercicio6 {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Digite o número: ");
+        int n = read.nextByte();
+        int a = n;
 
+        for (int i = 1; i < a; i++) {
+            n = n * (a-i);
+        }
+        System.out.println(n);
     }
 }
---
+++ src/main/java/vetores/Exercicio2.java
@@ -0,0 +1,7 @@
+package vetores;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        double[] number = new double[10];
+    }
+}
